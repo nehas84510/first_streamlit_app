b4a876ca62290eb85c155c1ed9797e5ffae4375b
nehas84510 <116145496+nehas84510@users.noreply.github.com>
2022-10-19 14:25:38 +0530

    Update streamlit_app.py
    correcting typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 
 streamlit.header('Breakfast Menu')
-streamlit.text0('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text(' 🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text(' 🐔 Hard-Boiled Free-Range Egg')
 streamlit.text(' 🥑🍞 Avocado Toast')
