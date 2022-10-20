f020abb1ea4386d3812d6e1a0fa66261ac3d756f
nehas84510 <116145496+nehas84510@users.noreply.github.com>
2022-10-20 14:26:08 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,7 +44,7 @@ streamlit.text("Hello from Snowflake:")
 streamlit.text(my_data_row)
 
 my_cur.execute("select * from fruit_load_list")
-my_display=my_cur.fetchone()
+my_display=my_cur.fetchall()
 streamlit.text("The fruit load list contains :")
 streamlit.dataframe(my_display)
 
